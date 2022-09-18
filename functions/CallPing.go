3415f79c51a251e2260aae22fabb7b8353626c5d
taubyte-test <dsad@dsadsa.caosd>
2022-09-18 06:25:33 -0500

    Add ping p2p method

---
+++ functions/CallPing.go
@@ -0,0 +1,37 @@
+package lib
+
+import (
+    "bitbucket.org/taubyte/go-sdk/event"
+)
+
+//export ping
+func ping(e event.Event) uint32 {
+    h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+	err = runPing(h)
+	if err != nil {
+		errString := fmt.Sprintf(`{"error": ping failed with %s}`, err)
+		h.Write([]byte(errString))
+		return 1
+	}
+
+	return 0
+}
+
+func runPing(h event.HttpEvent) error {
+	cmd, err := node.New("/test/v1").Command("ping")
+	if err != nil {
+		return err
+	}
+
+	response, err := cmd.Send([]byte("Hello, world"))
+	if err != nil {
+		return err
+	}
+
+	_, err = h.Write(response)
+	return err
+}
---
+++ functions/PingP2P.go
@@ -0,0 +1,54 @@
+package lib
+
+import (
+    "bitbucket.org/taubyte/go-sdk/event"
+    p2p "bitbucket.org/taubyte/go-sdk/p2p/event"
+)
+
+//export ping
+func ping(e event.Event) uint32 {
+    	p, err := e.P2P()
+	if err != nil {
+		return 1
+	}
+
+	err = runPing(p)
+	if err != nil {
+		errString := fmt.Sprintf(`{"error": ping failed with %s}`, err)
+		p.Write([]byte(errString))
+		return 1
+	}
+
+	return 0
+}
+
+func runPing(e p2p.Event) error {
+	command, err := e.Command()
+	if err != nil {
+		return err
+	}
+
+	data, err := e.Data()
+	if err != nil {
+		return err
+	}
+
+	from, err := e.From()
+	if err != nil {
+		return err
+	}
+
+	protocol, err := e.Protocol()
+	if err != nil {
+		return err
+	}
+
+	toWrite := fmt.Sprintf(`{
+		protocol: %s,
+		command: %s,
+		data: %s,
+		from: %s
+}`, protocol, command, string(data), from.String())
+
+	return e.Write([]byte(toWrite))
+}
